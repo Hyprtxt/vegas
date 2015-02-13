e279153b25c1c545e233dd1a09e73bfada4f5dad
Taylor Young <taylor@hyprtxt.com>
2015-02-13 13:18:26 -0600

    sites api

---
+++ api/controllers/SitesController.js
@@ -0,0 +1,11 @@
+/**
+ * SitesController
+ *
+ * @description :: Server-side logic for managing sites
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
